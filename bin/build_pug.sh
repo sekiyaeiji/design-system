b64001832c9acbdcdcbd93b52ed5a64ee8221c1a
e s <eiji@SazabyMBP.local>
2018-11-21 00:14:50 +0900

    shell add obj

--- bin/build_pug.sh
+++ bin/build_pug.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-JSON_DATA=src/pug/data/data.json
-JSON_VERSION=conf/version.json
+# JSON_DATA=`cat src/pug/data/data.json | jq .`
+# JSON_VERSION=`cat conf/version.json  | jq .`
+
+JSON_DATA=`cat src/pug/data/data.json`
+JSON_VERSION=`cat conf/version.json`
+
+echo "\n\n━━━━━━━━━"
+
+echo `cat src/pug/data/data.json`
+
+echo "\n\n━━━━━━━━━"
+
+echo $JSON_DATA
+
+echo "\n\n━━━━━━━━━"
+
+echo $JSON_VERSION
+
+echo "\n\n━━━━━━━━━"
+
 JSON_NEW=`jq -s add $JSON_DATA $JSON_VERSION`
+# echo $JSON_NEW
 
-# jq --help
-
-echo $JSON_NEW
-
-pug -O $JSON_NEW -P src/pug/page/ --out dist
+# pug -O $JSON_NEW -P src/pug/page/ --out dist
